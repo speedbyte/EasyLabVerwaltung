c7af1e905e99d5ac9a8096c32af298e24de2e9ff
itlx3354admin <itlx3354admin@hs-esslingen.de>
2017-10-09 19:08:58 +0200

    saving snapshot

---
+++ ezslab/mlab.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+
+import time
+import datetime
+import subprocess
+import gitlab
+import sys
+import os
+
+# gitlab v3
+
+gitlab_server_url = 'wwwitrt.hs-esslingen.de:8443'
+gitlab_server_token = 'Lzx9BbajbRkxhTZjknDt'
+user = 'ezslab'
+password = 'njn$43EL'
+
+
+def delete_repos(gl):
+    if ( mode == "fresh" ):
+        # -------------------------------------BACKUP
+        answer = raw_input("Do you want a backup of the GIT repos (Y/N)")
+        if answer == "Y":
+            print "Backup GIT repos.. This can take a long time...."
+        else:
+            print "No Backup GIT repos"
+
+        # -------------------------------------DELETE
+        response = raw_input(
+            "Proceed with delete (Y/N)")
+        if response == "Y":
+            for x in range(iterations):
+                for y in range(maxrepos):
+                    reponame = listofprojects[x][y]
+                    print "deleting repo " + reponame
+                    time.sleep(1)
+                    # Get a project by ID
+                    try:
+                        project = gl.projects.get('LaborAufgaben/' + reponame)
+                    except:
+                        print "No repo %s found under LaborAufgaben, Skipping....." % (reponame)
+                        continue
+                    project.delete()
+
+        time.sleep(1)
+
+
+def delete_repo(gl, reponame):
+    # Get a project by ID
+    try:
+	project = gl.projects.get('LaborAufgaben/' + reponame)
+	project.delete()
+    except:
+	print "No repo %s found under LaborAufgaben, Skipping....." % (reponame)
+
+
+def create_gitlab_repo(gl, reponame, templateURL):
+
+        # -------------------------------------CREATE and FILL
+        curdir_save = os.getcwd()
+        submodules_dir = curdir_save + '/' + '../submodules/template-repo'
+        os.chdir(submodules_dir)
+        print os.getcwd()
+        subprocess.check_call(["git", "remote", "-v"])
+	print "creating repo " + reponame
+	project = gl.projects.create({'name': reponame, 'namespace_id': 10})
+	time.sleep(1)
+	template_repo_original = templateURL
+	template_repo = "https://%s:%s@%s/LaborAufgaben/%s.git"% (user, password, gitlab_server_url, reponame)
+	command = "git remote set-url origin " + template_repo
+	print command
+	subprocess.check_call(command.split(' '))
+	subprocess.check_call("git push origin master".split(' '))
+	print "Adding users"
+	template_repo = "https://%s@%s/LaborAufgaben/%s.git" % (user, gitlab_server_url, reponame)
+	command = "git remote set-url origin " + template_repo_original
+	print command
+	subprocess.check_call(command.split(' '))
+	branch = project.branches.get('master')
+	branch.protect()
+	branch = project.branches.create({'branch_name': 'Work', 'ref': 'master'})
+
+        os.chdir(curdir_save)
+	return project
+
+
+def create_user(gl, membertoadd):
+
+	theUserList = gl.users.list(username=membertoadd)
+	if not theUserList: 
+		print "Creating user " + membertoadd + " here..."
+                userObject = {'email': membertoadd + '@hs-esslingen.de',
+			'password': 'ezsiscool',
+			'username': membertoadd,
+			'name': membertoadd}
+		theUser = gl.users.create(userObject)
+		print "Created new user in GITLAB", membertoadd, theUser.id
+	else:
+		theUser = theUserList[0]
+	return theUser.id
+
+
+def manage_repo(gl, groupname, groupmembers, reponame):
+	# -------------------------------------ADD MEMBERS
+	# both incremental and fresh
+	# Now find a project with the repo name
+	try:
+		repo = gl.projects.get('LaborAufgaben/' + reponame)
+		print "project found ", repo.id
+	except:#if ( repo == None ) :
+		print "create a repo with repo name" 
+		templateURL="https://vagrawal@gitlab.hs-esslingen.de/IT-Allgemein-LaborVerwaltung/template-repo.git"
+		repo = create_gitlab_repo(reponame,templateURL)
+		
+	actualmembers = repo.members.list()
+	if len(actualmembers) > 0:
+		for number_of_members in range(len(actualmembers)):
+			actualmembers[number_of_members].delete()
+ 	groupmembers = groupmembers.split(',')
+	for x in range(len(groupmembers)):
+		membername = groupmembers[x].strip()
+       		create_user(membername) # existing users are not re-created.
+	        memberfound = gl.users.list(username=membername)
+		time.sleep(1) 
+		print memberfound
+	        repo.members.create({'user_id': memberfound[0].id, 'access_level': gitlab.DEVELOPER_ACCESS})
+			
+	# End of create team groups.
+
+
+if __name__ == '__main__':
+	
+	gl = gitlab.Gitlab('https://' + gitlab_server_url, gitlab_server_token)
+	groupname = "atest_group_test1" #listofgroups[x][y]
+	groupmembers = "XXXXit00, YYYYit00" 
+	reponame = "ctest_repo_test1" #listofprojects[x][y]
+	manage_repo(gl, groupname, groupmembers, reponame)
+
+# curl --header "PRIVATE-TOKEN: <my token>" -X POST "https://gitlab.com/api/v3/projects?name=foobartest8&namespace_id=10"
+# curl --request POST --header "PRIVATE-TOKEN: $gitlab_server_TOKEN" https://gitlab.example.com/api/v3/projects/:id/members/:user_id?access_level=30
+# ldapadd -x -c -S logs/ldapmodify-error.log -D cn=admin,dc=hs-esslingen,dc=de -w marc276%! -f ldif-prepare-ezs-ldapm.ldif
+# curl --request POST --header "PRIVATE-TOKEN: pHNJ8ksUCCssCDmtyZxh"
+# https://wwwitrt.hs-esslingen.de:8443/api/v3/projects/:id/members/:user_id?access_level=30
+# ldapadd -x -c -S logs/ldapmodify-error.log -D cn=admin,dc=hs-esslingen,dc=de -w marc276%! -f ldif-prepare-ezs-ldapm.ldif
